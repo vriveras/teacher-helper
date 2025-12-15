1a74537598cefd7055e2b45ad3a4670c24db146c
Vicente Rivera <virivera@microsoft.com>
2025-12-15 08:19:45 -0800

    removing bad code from first commit

--- src/core/index.ts
+++
@@ -1,4 +0,0 @@
-export { BaseAgent } from './base-agent.js';
-export { EventBus, globalEventBus } from './event-bus.js';
-export { TaskQueue, globalTaskQueue } from './task-queue.js';
-export { StateManager, globalStateManager } from './state-manager.js';
